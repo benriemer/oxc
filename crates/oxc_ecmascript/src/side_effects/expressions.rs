9bb7746adc3d1e0a162d7f4c9f422a2ac3833aca
Dunqing <dengqing0821@gmail.com>
2026-03-11 10:11:13 +0800

    fix(ecmascript): remove `DataView`, as it would cause an error if the argument is not an `ArrayBuffer`

--- crates/oxc_ecmascript/src/side_effects/expressions.rs
+++ crates/oxc_ecmascript/src/side_effects/expressions.rs
@@ -300,7 +300,7 @@ fn is_pure_constructor(name: &str) -> bool {
             | "SyntaxError" | "TypeError" | "URIError" | "Number" | "Object" | "String"
             | "Int8Array" | "Uint8Array" | "Uint8ClampedArray" | "Int16Array" | "Uint16Array"
             | "Int32Array" | "Uint32Array" | "Float32Array" | "Float64Array"
-            | "BigInt64Array" | "BigUint64Array" | "DataView")
+            | "BigInt64Array" | "BigUint64Array")
 }
 
 /// Whether the name matches any known global constructors.
